cdac0b04e15a8cf10ce0d5cb1b3f8fd380218544
YoCatt <83766025+YoCatt@users.noreply.github.com>
2023-02-01 16:01:35 +0200

    Add 3 More Browsers
    so basically i added opera gx, opera and edge

--- LLL_MantisReadsYourMemory/Assets/Folders/Scripts/HistoryGetter.cs
+++ LLL_MantisReadsYourMemory/Assets/Folders/Scripts/HistoryGetter.cs
@@ -10,11 +10,15 @@ public class HistoryGetter
 {
     public static string chromeDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"\AppData\Local\Google\Chrome\User Data\Default\History";
     public static string braveDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\History";
-    public static string foxDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"C:\AppData\Roaming\Mozilla\Firefox\Profiles\1pkr17pg.default-release";
+    public static string gxDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"\AppData\Roaming\Opera Software\Opera GX Stable\History";
+    public static string operaDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"\AppData\Roaming\Opera Software\Opera Stable\History";
+    public static string edgeDefaultHistoryDir =>  @"C:\Users\" + GetUsername() + @"\AppData\Local\Microsoft\Edge\User Data\Default\History";
 
     public static string chromeHistoryCopyDir = Application.streamingAssetsPath + "/" + "ChromeHistory";
     public static string braveHistoryCopyDir = Application.streamingAssetsPath + "/" + "BraveHistory";
-    public static string foxHistoryCopyDir = Application.streamingAssetsPath + "/" + "FoxHistory";
+    public static string gxHistoryCopyDir = Application.streamingAssetsPath + "/" + "GxHistory";
+    public static string operaHistoryCopyDir = Application.streamingAssetsPath + "/" + "OperaHistory";
+    public static string edgeHistoryCopyDir = Application.streamingAssetsPath + "/" + "EdgeHistory";
 
     public static List<string> historyCopies;
 
@@ -30,7 +34,11 @@ public class HistoryGetter
         //deletes history files if they exist in Streaming Assets:
         System.IO.File.Delete(braveHistoryCopyDir);
         System.IO.File.Delete(chromeHistoryCopyDir);
+        System.IO.File.Delete(gxHistoryCopyDir);
+        System.IO.File.Delete(edgeHistoryCopyDir);
+        System.IO.File.Delete(operaHistoryCopyDir);
 
+        // copies the browser history file and adds it to the historyCopies list
         if (System.IO.File.Exists(chromeDefaultHistoryDir))
         {
             CopyChromeHistoryFile();
@@ -43,10 +51,22 @@ public class HistoryGetter
             historyCopies.Add(braveHistoryCopyDir);
         }
         
-        if (System.IO.File.Exists(chromeDefaultHistoryDir))
+        if (System.IO.File.Exists(gxDefaultHistoryDir))
         {
-            CopyChromeHistoryFile();
-            historyCopies.Add(chromeHistoryCopyDir);
+            CopyGxHistoryFile();
+            historyCopies.Add(gxHistoryCopyDir);
+        }
+        
+        if (System.IO.File.Exists(edgeDefaultHistoryDir))
+        {
+            CopyEdgeHistoryFile();
+            historyCopies.Add(edgeHistoryCopyDir);
+        }
+        
+        if (System.IO.File.Exists(operaDefaultHistoryDir))
+        {
+            CopyOperaHistoryFile();
+            historyCopies.Add(operaHistoryCopyDir);
         }
 
         return historyCopies;
@@ -69,6 +89,42 @@ public class HistoryGetter
         try
         {
             File.Copy(braveDefaultHistoryDir, braveHistoryCopyDir, true);
+        }
+        catch (IOException iox)
+        {
+            Debug.Log(iox.Message);
+        }
+    }
+    
+    public static void CopyGxHistoryFile()
+    {
+        try
+        {
+            File.Copy(gxDefaultHistoryDir, gxHistoryCopyDir, true);
+        }
+        catch (IOException iox)
+        {
+            Debug.Log(iox.Message);
+        }
+    }
+    
+    public static void CopyEdgeHistoryFile()
+    {
+        try
+        {
+            File.Copy(edgeDefaultHistoryDir, edgeHistoryCopyDir, true);
+        }
+        catch (IOException iox)
+        {
+            Debug.Log(iox.Message);
+        }
+    }
+    
+    public static void CopyOperaHistoryFile()
+    {
+        try
+        {
+            File.Copy(operaxDefaultHistoryDir, operaHistoryCopyDir, true);
         }
         catch (IOException iox)
         {
